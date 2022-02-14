d4a213ff575e4834cec7cf509eda27a9e1a46fa5
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-02-14 19:58:17 +0530

    Splash Screen Updated

--- Screens/splash_screen.py
+++ Screens/splash_screen.py
@@ -17,51 +17,55 @@ class SplashScreen(tk.Tk):
         self.PRIMARY_COLOUR = 'White'
         self.SECONDARY_COLOUR = '#249794'
 
-        self.WIDTH_SPLASH_SCREEN = 427
-        self.HEIGHT_SPLASH_SCREEN = 250
+        self.WIDTH_SPLASH_SCREEN = app.WIDTH
+        self.HEIGHT_SPLASH_SCREEN = app.HEIGHT - 100
         self.PIXEL_COUNT_WIDTH = self.winfo_screenwidth()
         self.PIXEL_COUNT_HEIGHT = self.winfo_screenheight()
         x_coordinate = (self.PIXEL_COUNT_WIDTH / 2) - (self.WIDTH_SPLASH_SCREEN / 2)
         y_coordinate = (self.PIXEL_COUNT_HEIGHT / 2) - (self.HEIGHT_SPLASH_SCREEN / 2)
         self.geometry("%dx%d+%d+%d" % (self.WIDTH_SPLASH_SCREEN, self.HEIGHT_SPLASH_SCREEN, x_coordinate, y_coordinate))
 
-        self.overrideredirect(1)
+        self.overrideredirect(True)  # removes the window options
 
         s = ttk.Style()
         s.theme_use('clam')
         s.configure("red.Horizontal.TProgressbar", foreground='red', background='#4f4f4f')
-        self.progress = Progressbar(self, style="red.Horizontal.TProgressbar", orient="horizontal", length=500,
-                                    mode='determinate', )
 
-        self.progress.place(x=-10, y=235)
+        # creator_label = ttk.Label(self, text='PROGRAMMED', foreground=self.PRIMARY_COLOUR, background=self.SECONDARY_COLOUR,
+        #                font=(self.FONT_STYLE, self.FONT_SIZE, self.FONT_WEIGHT))
+        # creator_label.place(x=50, y=110)
 
-        l1 = ttk.Label(self, text='SPLASH', foreground=self.PRIMARY_COLOUR, background=self.SECONDARY_COLOUR,
-                       font=(self.FONT_STYLE, self.FONT_SIZE, self.FONT_WEIGHT))
-        l1.place(x=50, y=80)
+        tk.Frame(self, width=self.WIDTH_SPLASH_SCREEN, height=self.HEIGHT_SPLASH_SCREEN,
+                 background=self.SECONDARY_COLOUR).place(x=0, y=0)
 
-        l2 = ttk.Label(self, text='SCREEN', foreground=self.PRIMARY_COLOUR, background=self.SECONDARY_COLOUR,
-                       font=(self.FONT_STYLE, self.FONT_SIZE, self.FONT_WEIGHT))
-        l2.place(x=155, y=82)
+        title_label = ttk.Label(self, text='MACHINET', foreground=self.PRIMARY_COLOUR, background=self.SECONDARY_COLOUR,
+                                font=(self.FONT_STYLE, self.FONT_SIZE + 40, self.FONT_WEIGHT))
+        title_label.place(x=100, y=200)
 
-        l3 = ttk.Label(self, text='PROGRAMMED', foreground=self.PRIMARY_COLOUR, background=self.SECONDARY_COLOUR,
-                       font=(self.FONT_STYLE, self.FONT_SIZE, self.FONT_WEIGHT))
-        l3.place(x=50, y=110)
+        sub_title_label = ttk.Label(self, text='An Auto ML Assist Tool', foreground=self.PRIMARY_COLOUR,
+                                    background=self.SECONDARY_COLOUR,
+                                    font=(self.FONT_STYLE, self.FONT_SIZE - 5, self.FONT_WEIGHT))
+        sub_title_label.place(x=100, y=280)
 
-        tk.Frame(self, width=427, height=241, background=self.SECONDARY_COLOUR).place(x=0, y=0)
-
-        b1 = tk.Button(self, width=10, height=1, text='Get Started', command=self.bar, border=0,
-                       foreground=self.SECONDARY_COLOUR,
-                       background=self.PRIMARY_COLOUR)
-        b1.place(x=170, y=200)
+        activate_button = tk.Button(self, width=10, height=1, text='Get Started', command=self.bar, border=0,
+                                    foreground=self.SECONDARY_COLOUR,
+                                    background=self.PRIMARY_COLOUR)
+        activate_button.place(x=450, y=520)
 
     def bar(self):
-        l4 = ttk.Label(self, text='Loading...', foreground='white', background=self.PRIMARY_COLOUR,
-                       font=(self.FONT_STYLE, 10, self.FONT_WEIGHT))
-        l4.place(x=18, y=210)
+        loading_label = ttk.Label(self, text='Loading...', foreground=self.SECONDARY_COLOUR,
+                                  background=self.PRIMARY_COLOUR,
+                                  font=(self.FONT_STYLE, 11, self.FONT_WEIGHT))
+        loading_label.place(x=3, y=self.HEIGHT_SPLASH_SCREEN - 42)
+
+        progress = Progressbar(self, style="red.Horizontal.TProgressbar", orient="horizontal",
+                               length=self.WIDTH_SPLASH_SCREEN,
+                               mode='determinate')
+        progress.place(x=0, y=self.HEIGHT_SPLASH_SCREEN - 18)
 
         r = 0
         for i in range(100):
-            self.progress['value'] = r
+            progress['value'] = r
             self.update_idletasks()
             time.sleep(0.03)
             r = r + 1
