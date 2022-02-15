be80b69735a1f3b432e24922bbe7e4065935a933
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2022-02-15 19:46:20 +0530

    Main updated

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 """Docstring"""
-#from Core import app
 from Screens import splash_screen
 from Utils.utilities import DpiAwareness
 
@@ -7,7 +6,3 @@ if __name__ == '__main__':
     splash = splash_screen.SplashScreen()
     DpiAwareness(splash)
     splash.mainloop()
-
-    # start = app.APP()
-    # DpiAwareness(app)
-    # start.mainloop()
